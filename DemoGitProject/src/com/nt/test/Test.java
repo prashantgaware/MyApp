d26c5c62cc3812a74f555e8056c878f9012f1b8e
dev <dev@gmail.com>
2023-09-30 18:11:16 +0530

    main method by dev

--- DemoGitProject/src/com/nt/test/Test.java
+++ DemoGitProject/src/com/nt/test/Test.java
@@ -1,5 +1,14 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Test {
+	
+	public static void main(String[] args) {
+		
+		Utility utility = new Utility();
+		
+		System.out.println(utility.getSysDate());
+	}
 
 }
